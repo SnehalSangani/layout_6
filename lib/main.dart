a003fb8278e909a1c6c4bcfc76f7cfa6d7d82d32
DELL <snehal18122004@gmail.com>
2023-02-05 11:43:58 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:layout_6/six.dart';
+
+
+
+
+void main()
+{
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: six(),
+      )
+  );
+}
